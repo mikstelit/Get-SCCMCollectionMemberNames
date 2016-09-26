fa7ba45f340d82363c56c31dff7919bad9f1b4f7
Mike.Littlefield@live.com <mike.littlefield@live.com>
2016-09-26 12:23:02 -0700

    Initial commit

---
+++ Get-SCCMCollectionMemberNames.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.Synopsis
+   Returns the names of members of an SCCM collection
+.DESCRIPTION
+   Returns the names of members of an SCCM collection
+.EXAMPLE
+   Get-SCCMCollectionMemberNames 'Test Collection'
+.EXAMPLE
+   Get-SCCMCollectionMemberNames -CollectionName 'Test Collection' `
+                             -SiteCode 'S1'
+#>
+Function Get-SCCMCollectionMemberNames
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]$CollectionName,
+
+        # Param2 help description
+        [Parameter(Mandatory=$false,
+                   Position=1)]
+        [string]$SiteCode = 'S1'
+    )
+
+    Try
+    {
+        Import-Module 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1' -ErrorAction Stop
+        CD "$($SiteCode):" -ErrorAction Stop
+        Get-CMCollectionMember -CollectionName $CollectionName -ErrorAction Stop | foreach { $_.Name } | Sort-Object
+    }
+    Catch
+    {
+        Write-Output $Error[0].Exception.Message
+    }
+}
